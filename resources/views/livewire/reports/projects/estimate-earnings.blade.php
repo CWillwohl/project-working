cb6056171878b8e9dbef0cdd9f65a4ce53e78de2
CWillwohl <developer.caiowillwohl@gmail.com>
2025-02-01 21:38:59 -0300

    Improve Estimate Earnings design

--- resources/views/livewire/reports/projects/estimate-earnings.blade.php
+++ resources/views/livewire/reports/projects/estimate-earnings.blade.php
@@ -5,21 +5,21 @@
             <div class="h-auto">
                 <h1 class="p-4 my-8 text-lg font-medium text-center border rounded text-primary">{{ $project->name ?? 'Selecione um Projeto' }}</h1>
                 <div class="flex flex-col w-full space-y-4">
-                    <div class="h-auto text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-primary/80 to-primary/60">
-                        <div class="flex-wrap text-sm text-white whitespace-pre-line stat-title">Valor recebido por hora trabalhada:</div>
-                        <div class="text-lg stat-value">
+                    <div class="h-auto rounded-lg border border-zinc-200 border-l-4 border-l-primary stat">
+                        <div class="flex-wrap text-sm whitespace-pre-line stat-title">Valor recebido por hora trabalhada:</div>
+                        <div class="text-lg text-slate-600 stat-value">
                         {{ $project->formatted_price_per_hour }}
                         </div>
                     </div>
-                    <div class="h-auto text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-green-700 to-green-500">
-                        <div class="flex-wrap text-sm text-white whitespace-pre-line stat-title">Total de valores recebidos:</div>
-                        <div class="text-lg stat-value">
+                    <div class="h-auto rounded-lg border border-zinc-200 border-l-4 border-l-green-500 stat">
+                        <div class="flex-wrap text-sm whitespace-pre-line stat-title">Total de valores recebidos:</div>
+                        <div class="text-lg text-slate-600 stat-value">
                         {{ $project->formatted_total_received }}
                         </div>
                     </div>
-                    <div class="h-auto text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-sky-700 to-sky-500">
-                        <div class="flex-wrap text-sm text-white whitespace-pre-line stat-title">Total de valores para receber:</div>
-                        <div class="text-lg stat-value">
+                    <div class="h-auto rounded-lg border border-zinc-200 border-l-4 border-l-sky-500 stat">
+                        <div class="flex-wrap text-sm whitespace-pre-line stat-title">Total de valores para receber:</div>
+                        <div class="text-lg text-slate-600 stat-value">
                         {{ $project->formatted_price_to_receive }}
                         </div>
                     </div>
@@ -42,7 +42,7 @@
                 <div class="flex flex-col items-center justify-center w-full gap-4">
                     <p class="text-base text-center">Quantas horas serão trabalhas por dia?</p>
                     <div class="flex items-center justify-center w-48 gap-4 p-4 border rounded-lg shadow">
-                        <p class="w-24 text-6xl font-medium">{{ $timeWorked }}</p>
+                        <p class="w-24 text-zinc-600 text-6xl font-medium">{{ $timeWorked }}</p>
                         <div class="h-16 border"></div>
                         <div class="flex flex-col items-center w-16 gap-2">
                             <button class="btn btn-md btn-primary" wire:click="incrementTimeWorked">
@@ -57,15 +57,15 @@
                 </div>
 
                 <div class="flex flex-col w-full space-y-4">
-                    <div class="h-auto text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-primary/80 to-primary/60">
-                        <div class="flex-wrap text-sm text-white whitespace-pre-line stat-title">Valor recebido por hora trabalhada:</div>
-                        <div class="text-lg stat-value">
+                    <div class="h-auto rounded-lg border border-zinc-200 stat border-r-4 border-r-primary">
+                        <div class="flex-wrap text-sm whitespace-pre-line stat-title">Valor recebido por hora trabalhada:</div>
+                        <div class="text-lg stat-value text-zinc-600">
                         {{ $project->formatted_price_per_hour }}
                         </div>
                     </div>
-                    <div class="h-auto text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-green-700 to-green-500">
-                        <p class="flex-wrap text-sm text-white whitespace-pre-line stat-title">Valor estimado no período selecionado:</p>
-                        <div class="text-lg stat-value">
+                    <div class="h-auto rounded-lg border border-zinc-200 stat border-r-4 border-r-green-500">
+                        <p class="flex-wrap text-sm whitespace-pre-line stat-title">Valor estimado no período selecionado:</p>
+                        <div class="text-lg stat-value text-zinc-600">
                             {{ $totalEarnings }}
                         </div>
                     </div>
@@ -77,23 +77,23 @@
     <div class="flex-col w-full gap-4 p-4 mt-8 bg-white rounded-lg shadow-lg">
         <h1 class="text-lg font-medium">Ganhos estimados: </h1>
         <div class="flex flex-col items-center justify-center w-full gap-4 mt-4 md:flex-row">
-            <div class="text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-primary/80 to-primary/60">
-                <div class="text-sm whitespace-pre-line first:text-white stat-title">Valor recebido por um dia:</div>
-                <div class="text-xl md:text-4xl stat-value">
+            <div class="rounded-lg border border-zinc-200 border-l-4 border-l-primary stat">
+                <div class="text-sm whitespace-pre-line stat-title mb-1">Valor recebido por um dia:</div>
+                <div class="text-xl md:text-4xl stat-value text-slate-600">
                     {{ isset($estimatedValues['Day']) ? $estimatedValues['Day'] : 'R$ 0,00' }}
                 </div>
             </div>
 
-            <div class="text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-primary/80 to-primary/60">
-                <div class="text-sm whitespace-pre-line first:text-white stat-title">Valor recebido por 15 dias:</div>
-                <div class="text-xl md:text-4xl stat-value">
+            <div class="rounded-lg border border-zinc-200 border-l-4 border-l-primary stat">
+                <div class="text-sm whitespace-pre-line stat-title mb-1">Valor recebido por 15 dias:</div>
+                <div class="text-xl md:text-4xl stat-value text-slate-600">
                     {{ isset($estimatedValues['15Days']) ? $estimatedValues['15Days'] : 'R$ 0,00' }}
                 </div>
             </div>
 
-            <div class="text-white rounded-lg shadow-lg border-zinc-700 stat bg-gradient-to-b from-primary/80 to-primary/60">
-                <div class="text-sm whitespace-pre-line first:text-white stat-title">Valor recebido por 30 dias:</div>
-                <div class="text-xl md:text-4xl stat-value">
+            <div class="rounded-lg border border-zinc-200 border-l-4 border-l-primary stat">
+                <div class="text-sm whitespace-pre-line stat-title mb-1">Valor recebido por 30 dias:</div>
+                <div class="text-xl md:text-4xl stat-value text-slate-600">
                     {{ isset($estimatedValues['Monthly']) ? $estimatedValues['Monthly'] : 'R$ 0,00' }}
                 </div>
             </div>
